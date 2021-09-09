172fb595cbd937e3c7cad37dc5e2febb6c3a5d9a
RytisMitkus <rytis@mitkus.lt>
2021-09-09 20:10:20 +0300

    removed google auth

--- packages/client/src/components/SignUp.vue
+++ packages/client/src/components/SignUp.vue
@@ -1,14 +1,24 @@
 <template>
-  <a href="/api/auth/google">auth google</a>
+  <buttn @click="signIn">auth</buttn>
+  <div>{{ auth }}</div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'signIn',
   data() {
     return {
       auth: null,
     }
+  },
+  methods: {
+    async signIn() {
+      const { data } = await axios.get('/api/auth/signin')
+      console.log(data)
+      this.auth = data.message
+    },
   },
 }
 </script>
--- packages/server/routes/AuthRouter.js
+++ packages/server/routes/AuthRouter.js
@@ -3,31 +3,13 @@ const passport = require('passport');
 
 const router = express.Router()
 
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    proxy: true
-},
-    (accessToken, refreshToken, profile, cb) => {
-        console.log(accessToken);
-        return cb(null, profile);
+router.get('/signin',
+    (req, res, next) => {
+        res.json({
+            message: 'signin'
+        })
     }
-));
-
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-
-
-router.get('/google/callback',
-    passport.authenticate('google', { failureRedirect: '/hey', session: false }),
-    (req, res) => {
-
-        res.send(req.user);
-    }
-
 );
 
 module.exports = router;
